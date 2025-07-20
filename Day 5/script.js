b7e369c2ff5741a6686b334864c89ba38833ae6c
Viren Soni <virensoni2901@gmail.com>
2025-07-20 13:35:45 +0530

    Ended Day 5 and starting Day 6

--- Day 5/script.js
+++ Day 5/script.js
@@ -1,1 +1,12 @@
-alert("Hello World")
+// const pi = 3.14
+// let radius = 7
+// let area = pi*radius*radius
+
+// console.log("Area of circle:", area)
+
+let mySmartPhone = {
+    name:"iPhone",
+    brand:"Apple",
+    platform:"iOS",
+    price:50000
+};
